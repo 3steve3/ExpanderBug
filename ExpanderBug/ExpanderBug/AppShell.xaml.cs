849f6fd249b4dd77ea43b05fc470d35275788cfa
Steve <stna7972@colorado.edu>
2022-06-17 03:08:41 -0600

    Add project files.

---
+++ ExpanderBug/ExpanderBug/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpanderBug
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
